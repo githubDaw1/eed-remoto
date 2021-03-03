a88aa247aeac9425415214a4668dd03191d8a96b
githubDaw1 <72400915+githubDaw1@users.noreply.github.com>
2021-03-03 21:06:53 +0100

    Add files via upload

---
+++ JavaGit/src/javaGit.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/** @author 34609 **/
+public class javaGit {
+
+	/** @param args **/
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.print ("Primer commit");
+		
+		System.out.println ();
+		
+		int suma = 3 * 5;
+		System.out.print ("Suma: " + suma);
+		
+		System.out.println ();
+		
+		int resta = 8 - 2;
+		System.out.print ("Resta: " + resta);
+		
+		System.out.println ();
+		
+		int multiplicacion = 6 * 4;
+		System.out.print ("Multiplicación: + multiplicacion");
+	}
+
+}
